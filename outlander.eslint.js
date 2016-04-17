4e1855e714387a01c5a141f88f6874455a78c70e
StreetStrider <teamfortresslife@gmail.com>
2016-04-17 16:17:32 +0300

    more rules.

--- outlander.eslint.js
+++ outlander.eslint.js
@@ -26,6 +26,7 @@ module.exports =
 		'quote-props': [ 2, 'consistent-as-needed' ],
 
 		'brace-style': [ 2, 'allman', { allowSingleLine: true } ],
+		curly: [ 2, 'all' ],
 
 		'keyword-spacing': 2,
 		'block-spacing': [ 2, 'always' ],
@@ -41,6 +42,10 @@ module.exports =
 			mode: 'minimum'
 		}],
 
+		'dot-notation': 2,
+
+		'dot-location': [ 2, 'property' ],
+
 		'space-unary-ops': [ 2, { words: true, nonwords: true } ],
 
 		'space-before-function-paren': [ 2, 'always' ],
@@ -51,21 +56,32 @@ module.exports =
 		'one-var-declaration-per-line': [ 2, 'always' ],
 		'newline-after-var': [ 2, 'always' ],
 
+		'wrap-iife': [ 2, 'outside' ],
+
 		'no-delete-var': 2,
 		'no-undef': 2,
+		'no-undef-init': 2,
 		'no-undefined': 2,
 		'no-unused-vars': 2,
 		'no-cond-assign': 2,
 		'no-debugger': 2,
 		'no-irregular-whitespace': 2,
 		'no-negated-in-lhs': 2,
+		'no-return-assign': 2,
 		'no-sparse-arrays': 2,
 		'no-unexpected-multiline': 2,
 		'no-underscore-dangle': 2,
 		'no-unreachable': 2,
 		'no-multi-str': 2,
 		'no-nested-ternary': 2,
+		'no-with': 2,
 		'valid-typeof': 2,
+
+		/* eqeqeq: 1, */
+		'no-fallthrough': 1,
+		'no-extra-bind': 1,
+		'no-useless-call': 1,
+		'no-useless-escape': 1,
 
 		'max-depth': [ 1, 3 ],
 		'max-len': [ 1, 80, 3 ],
