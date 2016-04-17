12ee5113d42bfd2a2a6e00ee9a0492ca0b301c94
StreetStrider <teamfortresslife@gmail.com>
2016-04-17 16:49:36 +0300

    minor imprs for dots, commas, braces, lengths.

--- outlander.eslint.js
+++ outlander.eslint.js
@@ -21,12 +21,15 @@ module.exports =
 		'eol-last': 2,
 
 		 semi:  [ 2, 'never' ],
-		'comma-style': [ 2, 'last'],
+
+		'comma-style': [ 2, 'last' ],
+		'comma-spacing': [ 2, { before: false, after: true } ],
+
 		 quotes: [ 2, 'single', { avoidEscape: true, allowTemplateLiterals: true } ],
 		'quote-props': [ 2, 'consistent-as-needed' ],
 
+		 curly: [ 2, 'all' ],
 		'brace-style': [ 2, 'allman', { allowSingleLine: true } ],
-		curly: [ 2, 'all' ],
 
 		'keyword-spacing': 2,
 		'block-spacing': [ 2, 'always' ],
@@ -43,10 +46,12 @@ module.exports =
 		}],
 
 		'dot-notation': 2,
-
 		'dot-location': [ 2, 'property' ],
+		'no-whitespace-before-property': 2,
+		'newline-per-chained-call': 1,
 
 		'space-unary-ops': [ 2, { words: true, nonwords: true } ],
+		'space-infix-ops': 2,
 
 		'space-before-function-paren': [ 2, 'always' ],
 		'no-spaced-func': 2,
@@ -55,6 +60,8 @@ module.exports =
 		'one-var': [ 2, 'never' ],
 		'one-var-declaration-per-line': [ 2, 'always' ],
 		'newline-after-var': [ 2, 'always' ],
+
+		'newline-before-return': 2,
 
 		'wrap-iife': [ 2, 'outside' ],
 
@@ -83,10 +90,12 @@ module.exports =
 		'no-useless-call': 1,
 		'no-useless-escape': 1,
 
+		'id-length': [ 1, { min: 1, max: 18 } ],
 		'max-depth': [ 1, 3 ],
 		'max-len': [ 1, 80, 3 ],
 		'max-params': [ 1, 3 ],
 		'max-nested-callbacks': [ 1, 3 ],
 		'max-statements': [ 1, 20 ],
+		'max-statements-per-line': [ 1, { max: 1 } ]
 	},
 }
