42008dcd2f66f996ee3861afd2b7dcb8cff60e36
Strider <strange.transistor@gmail.com>
2018-11-04 20:13:06 +0300

    note plugins topic.

--- outlander-flow.eslint.js
+++ outlander-flow.eslint.js
@@ -12,6 +12,9 @@ module.exports =
 
 	plugins:
 	[
+		// https://github.com/eslint/eslint/issues/10644
+		// https://github.com/eslint/eslint/issues/10644#issuecomment-430726105
+
 		// require.resolve('eslint-plugin-flowtype'),
 		// './node_modules/eslint-plugin-flowtype',
 		'flowtype',
