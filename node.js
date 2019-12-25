6a1e72ee3ee422622f4d788aed37f93bdd31e74e
Strider <strange.transistor@gmail.com>
2019-12-25 18:05:11 +0300

    more node rules.

--- node.js
+++ node.js
@@ -21,6 +21,9 @@ module.exports =
 
 	rules:
 	{
+		'node/no-unpublished-bin': 2,
+		'node/process-exit-as-throw': 2,
+
 		'node/no-unsupported-features/es-builtins': 2,
 		'node/no-unsupported-features/es-syntax':
 		[
@@ -29,7 +32,16 @@ module.exports =
 		'node/no-unsupported-features/node-builtins': 2,
 		'node/no-deprecated-api': 1,
 
+		'node/exports-style': [ 2, 'module.exports' ],
 		'node/no-exports-assign': 2,
-		'node/process-exit-as-throw': 2,
+
+		'node/no-extraneous-import': 1,
+		'node/no-extraneous-require': 1,
+
+		'node/no-missing-import': 1,
+		'node/no-missing-require': 1,
+
+		'node/no-unpublished-import': 1,
+		'node/no-unpublished-require': 1,
 	},
 }
